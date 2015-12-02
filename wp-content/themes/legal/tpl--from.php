21fbe65138bbd23827395e069130dff5027fe21c
andy <s.anmoldeep1993@gmail.com>
2015-12-02 16:17:33 +0530

    Added  @

---
+++ wp-content/themes/legal/tpl--from.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  * Template Name: TEst
+  */
+get_header();
+?>
+    <form action="" class="xsAjax" method="post">
+        <input type="hidden" name="action" value="add_test" id=""> <input type="text" name="" id="">
+        <button type="submit">ADD</button>
+    </form><?php get_footer();
