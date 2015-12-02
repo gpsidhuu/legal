67807af5faa663ca1287327df77851848334823d
gp <gpsidhuu@gmail.com>
2015-12-02 04:50:49 -0600

    Add Form Process JS

--- wp-content/themes/legal/js/global.js
+++ wp-content/themes/legal/js/global.js
@@ -17,6 +17,50 @@
                 $(this).parent().addClass('is-active');
             else
                 $(this).parent().removeClass('is-active')
-        })
+        });
+        ////  FORM PROCESS  ////
+        var formErrors;
+        var $ajaxForm = $('.xsAjax');
+        $ajaxForm.append('<div class="row"><div class="col-sm-12"><div class="formErrors"></div></div></div>');
+        $ajaxForm.submit(function (e) {
+            formErrors = new Array();
+            $('.formErrors').html('').removeClass('alert alert-danger alert-success');
+            var $this = $(this);
+            var $btn = $('button[type=submit]', this);
+            var $btnTxt = $btn.text();
+            $btn.addClass('xs-loading');
+            $btn.text('Loading ...');
+            startAjax($this);
+            $.ajax({
+                type: 'POST',
+                data: $(this).serialize(),
+                //dataType: 'JSON',
+                success: function (data) {
+                    stopAjax();
+                    if (isJson(data)) {
+                        var obj = jQuery.parseJSON(data);
+                        if (obj.status) {
+                            $('.formErrors').html(obj.errors).addClass('alert alert-success').removeClass(' alert-danger');
+                        } else {
+                            $('.formErrors').html(obj.errors).addClass('alert alert-danger').removeClass(' alert-success');
+                        }
+                        if (typeof  obj.redirect != 'undefined' && obj.redirect != null) {
+                            window.location = obj.redirect;
+                        }
+                        $btn.text($btnTxt);
+                        $btn.removeClass('xs-loading');
+                    } else {
+                        formErrors.push('Invalid JSON data returned by server');
+                        $('.formErrors').html(formErrors.join('<br>')).addClass('alert alert-danger').removeClass(' alert-success');
+                        $btn.text($btnTxt);
+                        $btn.removeClass('xs-loading');
+                    }
+                },
+                error: function () {
+                    stopAjax();
+                }
+            })
+            e.preventDefault();
+        });
     })
 })(jQuery)
